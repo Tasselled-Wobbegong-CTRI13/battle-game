23fc753185071b0db5484a058e87ca95bf9686cc
Christian Ashley <ashleycz1993@gmail.com>
2023-01-02 14:19:18 -0500

    Added expiration (maxAge) on cookie in cookieController

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -3,7 +3,7 @@ const cookieController = {};
 cookieController.setSSIDCookie = (req, res, next) => {
     // write code here
     if(req.cookies.ssid === undefined) {
-      res.cookie('ssid', res.locals.userId, { httpOnly: true });
+      res.cookie('ssid', res.locals.userId, { maxAge: 30*1000, httpOnly: true });
       return next();
     } else {
       return next();
