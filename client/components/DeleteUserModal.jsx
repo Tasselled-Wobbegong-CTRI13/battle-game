5f84424b2b76bc2c68aa53194f6436f7d3507976
Terrence Granger <terrence.granger@gmail.com>
2023-01-02 11:15:02 -0500

    added error messages for login / delete user forms

--- client/components/DeleteUserModal.jsx
+++ client/components/DeleteUserModal.jsx
@@ -1,44 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAuth from './useAuth.jsx';
 
 const DeleteUserModal = ({ displayState }) => {
+  const navigate = useNavigate();
+  const { setAuth } = useAuth();
+  const [deleteErrorModalStatus, setDeleteErrorModalStatus] =
+    useState('hidden');
 
-    const navigate = useNavigate();
-    const { setAuth } = useAuth();
+  const deleteErrorModalStyle = {
+    color: 'red',
+    visibility: deleteErrorModalStatus,
+    position: 'relative',
+    top: '-15px',
+  };
 
-    async function deleteUser(username, password, event) {
-        try {
-        event.preventDefault();
-        // fetch(`/api/users?username=${username}&password=${password}`, {
-        fetch(`/api/users?${new URLSearchParams({username, password}).toString()}`, {
-            method: 'DELETE',
-        })
-        .then(res => res.json())
-        .then(data => {
+  async function deleteUser(username, password, event) {
+    try {
+      event.preventDefault();
+      fetch(
+        `/api/users?${new URLSearchParams({ username, password }).toString()}`,
+        {
+          method: 'DELETE',
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
           console.log(data);
           if (data.success) {
-            setAuth({username: null});
+            setAuth({ username: null });
             navigate('/');
+          } else {
+            setDeleteErrorModalStatus('visible');
           }
         })
-        .catch(err => console.error(err));
-      } catch(err){
-        console.error(err);
-      }
+        .catch((err) => console.error(err));
+    } catch (err) {
+      console.error(err);
     }
+  }
 
-    return (
-          <form style={{display: displayState ? 'block' : 'none'} } className="delete-user-form" onSubmit={(e) => deleteUser(document.querySelector('#username').value, document.querySelector('#password').value, e)}>
-            <h4>Are you sure? Enter username and password below and confirm.</h4>
-            <hr />
-            <label htmlFor="username">Username</label>
-            <input className="inputClass" id="username" type="text" name="username" />
-            <label htmlFor="password" type="password">Password</label>
-            <input className="inputClass" id="password" type="password" name="password" />
-            <button className="delete-btn" type="submit">Confirm Delete</button>    
-          </form>
-    )
-}
+  return (
+    <form
+      style={{ display: displayState ? 'block' : 'none' }}
+      className="delete-user-form"
+      onSubmit={(e) =>
+        deleteUser(
+          document.querySelector('#username').value,
+          document.querySelector('#password').value,
+          e
+        )
+      }
+    >
+      <h4>Are you sure? Enter username and password below and confirm.</h4>
+      <hr />
+      <label htmlFor="username">Username</label>
+      <input className="inputClass" id="username" type="text" name="username" />
+      <label htmlFor="password" type="password">
+        Password
+      </label>
+      <input
+        className="inputClass"
+        id="password"
+        type="password"
+        name="password"
+      />
+      <p style={deleteErrorModalStyle}>Invalid username / password</p>
+      <button className="delete-btn" type="submit">
+        Confirm Delete
+      </button>
+    </form>
+  );
+};
 
 export default DeleteUserModal;
--- client/components/LoginPage.jsx
+++ client/components/LoginPage.jsx
@@ -1,47 +1,87 @@
 import React, { useState } from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import useAuth from './useAuth.jsx';
 
 const LoginPage = (props) => {
+  const navigate = useNavigate();
+  const { setAuth } = useAuth();
+  const [errorModalStatus, setErrorModalStatus] = useState('hidden');
 
-    const navigate = useNavigate();
-    const { setAuth } = useAuth();
-
-    async function login(username, password, event) {
-        try {
-        event.preventDefault();
-        // fetch(`/api/users?username=${username}&password=${password}`, {
-        fetch(`/api/users?${new URLSearchParams({username, password}).toString()}`, {
-            method: 'GET',
-            headers: {'Content-Type': 'application/json'},
-        })
-        .then(res => res.json())
-        .then(data => {
+  async function login(username, password, event) {
+    try {
+      event.preventDefault();
+      fetch(
+        `/api/users?${new URLSearchParams({ username, password }).toString()}`,
+        {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
           if (data.success) {
-            setAuth({username: data.username});
+            setAuth({ username: data.username });
             navigate('/gameboard');
+          } else {
+            setErrorModalStatus('visible');
           }
         })
-        .catch(err => console.error(err));
-      } catch(err){
-        console.error(err);
-      }
+        .catch((err) => console.error(err));
+    } catch (err) {
+      console.error(err);
     }
+  }
 
-    return (
-        <div className="containerBox">
-          <h1 className="title">Battle Game</h1>
-          <form className="login-form" onSubmit={(e) => login(document.querySelector('#username').value, document.querySelector('#password').value, e)}>
-            <label htmlFor="username">Username</label>
-            <input className="inputClass" id="username" type="text" name="username" />
-            <label htmlFor="password" type="password">Password</label>
-            <input className="inputClass" id="password" type="password" name="password" />
-            <button className="login-btn" type="submit">Login</button>    
-            <button className="signup-btn" type="button" onClick={() => navigate('/signup')}>Sign Up</button>
-          </form>
-        </div>
+  const errorModalStyle = {
+    color: 'red',
+    visibility: errorModalStatus,
+    position: 'relative',
+    top: '-15px',
+  };
 
-    )
-}
+  return (
+    <div className="containerBox">
+      <h1 className="title">Battle Game</h1>
+      <form
+        className="login-form"
+        onSubmit={(e) =>
+          login(
+            document.querySelector('#username').value,
+            document.querySelector('#password').value,
+            e
+          )
+        }
+      >
+        <label htmlFor="username">Username</label>
+        <input
+          className="inputClass"
+          id="username"
+          type="text"
+          name="username"
+        />
+        <label htmlFor="password" type="password">
+          Password
+        </label>
+        <input
+          className="inputClass"
+          id="password"
+          type="password"
+          name="password"
+        />
+        <p style={errorModalStyle}>Invalid username / password</p>
+        <button className="login-btn" type="submit">
+          Login
+        </button>
+        <button
+          className="signup-btn"
+          type="button"
+          onClick={() => navigate('/signup')}
+        >
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default LoginPage;
