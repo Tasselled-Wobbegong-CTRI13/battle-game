52e92c989904f0324cb7c42d0f4e4de29aeae188
Christian Ashley <ashleycz1993@gmail.com>
2023-01-02 10:01:51 -0500

    Cleanup of gameReducer

--- client/game-logic/gameReducer.js
+++ client/game-logic/gameReducer.js
@@ -1,6 +1,7 @@
 
 
 const gameReducer = (state, action) => {
+    // TODO: consider update passing a deep copy to various functions
     const newState = {messages: [...state.messages], gameOver: state.gameOver};
     switch (action.type) {
         case 'PLAYER_ATTACK': {
@@ -17,7 +18,7 @@ const gameReducer = (state, action) => {
             newState.player = {...state.player};
             newState.messages.push(`${newState.enemy.type} attacks you`);
             const result = action.payload(newState.enemy, newState.player);
-            if (typeof result === 'number') newState.messages.push(`The ${newState.enemy.type} loses ${result} hit points!`);
+            if (typeof result === 'number') newState.messages.push(`You lose ${result} hit points!`);
             else newState.messages.push(result)
             break;
         }
